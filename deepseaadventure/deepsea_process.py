0c46de81b80a1107a7e6f55121ea8a4b7d458e82
norman_cheng <norman_cheng@paradise-soft.com.tw>
2022-07-22 19:18:50 +0800

    deepsea down

--- deepseaadventure/deepsea_process.py
+++ deepseaadventure/deepsea_process.py
@@ -25,16 +25,23 @@ TEMP = {
     # -----------------------------------------------------------
     "lighting": os.path.join(TARGET_DIR, r"lighting.jpg"),
     "loop": os.path.join(TARGET_DIR, r"loop.jpg"),
+    "autoloop": os.path.join(TARGET_DIR, r"autoloop.jpg"),
+    "autostart": os.path.join(TARGET_DIR, r"autostart.jpg"),
+    "autocancel": os.path.join(TARGET_DIR, r"autocancel.jpg"),
     "contents": os.path.join(TARGET_DIR, r"contents.jpg"),
     "lobby": os.path.join(TARGET_DIR, r"lobby.jpg"),
     "record": os.path.join(TARGET_DIR, r"record.jpg"),
+    "recordcancel": os.path.join(TARGET_DIR, r"recordcancel.jpg"),
     "rule": os.path.join(TARGET_DIR, r"rule.jpg"),
+    "ruleswitch": os.path.join(TARGET_DIR, r"ruleswitch.jpg"),
+    "rulecancel": os.path.join(TARGET_DIR, r"rulecancel.jpg"),
     "musicsetting": os.path.join(TARGET_DIR, r"musicSetting.jpg"),
+    "settingcancel": os.path.join(TARGET_DIR, r"settingcancel.jpg"),
     "cancel": os.path.join(TARGET_DIR, r"contents_cancel.jpg"),
 }
 
 class deepseaImagePos(procedures.procedures):
-    def universal(self, device, path, sign=None):
+    def universal(self, device, path, sign=None, touch=True):
         # device: Phone
         # path: snapshot download folder path
         for _ in range(3):
@@ -46,18 +53,27 @@ class deepseaImagePos(procedures.procedures):
             if match_result != None:
                 break
             time.sleep(2)
+        if touch == True:
+            if match_result != None:
+                position = TargetPos().getXY(match_result, 5)
+                device.touch(position)
+                time.sleep(10)
+                return position
 
-        if match_result != None:
-            position = TargetPos().getXY(match_result, 5)
-            device.touch(position)
-            time.sleep(5)
-            return position
+            elif match_result == None:
+                # raise EOFError("手機當前頁面上沒有找到正確的圖片")
+                errortarget = os.path.basename(sign)
+                print("手機當前頁面上沒有找到正確的圖片_%s" %errortarget)
+                return None
+        elif touch == False:
+            if match_result != None:
+                position = TargetPos().getXY(match_result, 5)
+                return position
 
-        elif match_result == None:
-            # raise EOFError("手機當前頁面上沒有找到正確的圖片")
-            print("手機當前頁面上沒有找到正確的圖片_")
-            return None
-        
+            elif match_result == None:
+                errortarget = os.path.basename(sign)
+                print("手機當前頁面上沒有找到正確的圖片_%s" %errortarget)
+                return None
 
 def deepseaFlow(device, path):
     # 旋轉
@@ -66,20 +82,37 @@ def deepseaFlow(device, path):
     additionPosition = deepseaImagePos().universal(device, path, TEMP["addition"])
     # 金額"-"
     subtractionPosition = deepseaImagePos().universal(device, path, TEMP["subtraction01"])
-    # 快速按鈕
+    # 快速按鈕(highlight)
     lightingPosition = deepseaImagePos().universal(device, path, TEMP["lighting"])
+    # # 取消快速按鈕(no highlight)
+    # lightingPosition = deepseaImagePos().universal(device, path, TEMP["lighting"])
     # 循環按鈕
     loopPosition = deepseaImagePos().universal(device, path, TEMP["loop"])
+    # 循環設定頁
+    autoloopPosition = deepseaImagePos().universal(device, path, TEMP["autoloop"], touch=False)
+    # 循環開始
+    autostartPosition = deepseaImagePos().universal(device, path, TEMP["autostart"])
+    time.sleep(150)
+    # # 循環設定頁_取消
+    # autocancelPosition = deepseaImagePos().universal(device, path, TEMP["autocancel"])
     # 目錄按鈕
     contentsPosition = deepseaImagePos().universal(device, path, TEMP["contents"])
     # 返回KK大廳
-    lobbyPosition = deepseaImagePos().universal(device, path, TEMP["lobby"])
+    lobbyPosition = deepseaImagePos().universal(device, path, TEMP["lobby"], touch=False)
     # 注單記錄
-    # recordPosition = deepseaImagePos().universal(device, path, TEMP["record"])
+    recordPosition = deepseaImagePos().universal(device, path, TEMP["record"])
+    # 注單記錄_取消
+    recordcancelPosition = deepseaImagePos().universal(device, path, TEMP["recordcancel"])
     # 規則頁
-    # rulePosition = deepseaImagePos().universal(device, path, TEMP["rule"])
+    rulePosition = deepseaImagePos().universal(device, path, TEMP["rule"])
+    # 規則頁切換
+    ruleswitchPosition = deepseaImagePos().universal(device, path, TEMP["ruleswitch"])
+    # 規則頁_取消
+    rulecancelPosition = deepseaImagePos().universal(device, path, TEMP["rulecancel"])
     # 音樂設定
     settingPosition = deepseaImagePos().universal(device, path, TEMP["musicsetting"])
+    # 音樂設定_取消
+    settingcancelPosition = deepseaImagePos().universal(device, path, TEMP["settingcancel"])
     # cancel
     cancelPosition = deepseaImagePos().universal(device, path, TEMP["cancel"])
     
